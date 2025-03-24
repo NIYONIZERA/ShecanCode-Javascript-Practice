691f4c663e3b0f827e0a7ead90ad81e937ef0aaf
NIYONIZERA <benigneniyonizera58@gmail.com>
2025-03-24 10:17:24 +0200

    Simple math calculator

---
+++ Simple-math-calculator.js
@@ -0,0 +1,11 @@
+const a = 10; // Define the first number
+const b = 5;  // Define the second number
+var sum=a+b;
+var diff=a-b;
+var product=a*b;
+var quotient=a/b;
+console.log('Sum:', sum);
+console.log('Difference:', diff);
+console.log('Product:',product );
+console.log('Quotient:', quotient);
+console.log(`The sum of ${a} and ${b} is ${sum},The difference of ${a} and ${b} is ${diff},The product of ${a} and ${b} is ${product},The quotient of ${a} and ${b} is ${quotient} `);
